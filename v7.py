45ecfe84803166fbfeeddd9f96df8499584896b2
Vova Merkulov <vova.merkulov.09@mail.ru>
2023-09-17 13:52:58 +0700

    first commit

---
+++ v7.py
@@ -0,0 +1,6 @@
+import math
+a = float(input('Введите a: '))
+z1 = math.cos(3/8*math.pi-a/4)**2 - math.cos(11/8*math.pi+a/4)**2
+z2 = math.sqrt(2/2)*math.sin(a/2)
+print(f'z1 = {z1}')
+print(f'z2 = {z2}')
